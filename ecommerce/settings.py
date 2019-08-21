ac9af90f1a98e447b467fbc4251d700c1bdd3700
Liam O'Connor <liamoconnor8481@gmail.com>
2019-08-21 14:45:22 +0100

    added travis yml p5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 import dj_database_url
 
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
