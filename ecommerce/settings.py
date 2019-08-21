9ad2990f39a27b156827b7f9994ab0e9c3b32c1a
Liam O'Connor <liamoconnor8481@gmail.com>
2019-08-21 15:09:52 +0100

    added a Procfile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -97,18 +97,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+
 if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
         }
 else:
-    print('Postgres URL not found, using sqlite instead')
+    print('Database URL not found, using sqlite instead')
     DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
