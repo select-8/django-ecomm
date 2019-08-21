021716d35ae161fe7b9619b34aa231b9796a05c6
Liam O'Connor <liamoconnor8481@gmail.com>
2019-08-21 14:54:33 +0100

    added travis yml p7

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -103,7 +103,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {'default': dj_database_url.parse(os.getenv('DATABASE_URL'))}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
+        }
+else:
+    print('Postgres URL not found, using sqlite instead')
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
